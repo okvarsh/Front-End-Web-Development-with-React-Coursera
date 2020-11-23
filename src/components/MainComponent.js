f968efeb60eb2ae34a592a6a18a6a2d76d5d6645
okvarsh <varshvilas@gmail.com>
2020-11-23 16:24:54 +0530

    single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -59,7 +59,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route path="/menu/:dishId" component={(this.DishWithId)} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
